d6db29d32786172bb88952b5df67606d20aacfab
SimeonStoykov <simeon_stoykov@mail.bg>
2015-01-29 15:00:22 +0200

    Added info

--- UniSurvivalHorror/src/LevelUNWE.java
+++ UniSurvivalHorror/src/LevelUNWE.java
@@ -1,8 +1,8 @@
 
 public class LevelUNWE {
 	public static void Execute(Hero player){
-		System.out.println("test2");
-		System.out.println("Ehooo");
+		System.out.println("Very bad choice");
+		
 	}
 }
 
